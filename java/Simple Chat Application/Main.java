6889d56338ca100a1caf5c54526ebe3cd18b9f64
Blessed RT Chitambira <russeltinoble@gmail.com>
2019-03-24 21:43:20 +0200

    first commit

---
+++ java/Simple Chat Application/Main.java
@@ -0,0 +1,18 @@
+/*
+    Main Class
+ */
+
+
+/**
+ *
+ * @author russel
+ */
+public class Main 
+{
+    public static void main(String [] args)
+    {
+        MainFrame mainFrame = new MainFrame(); //create the MainFrame object
+        Thread thread  = new Thread(mainFrame); //run in a new thread
+        thread.start(); //start the thread
+    }
+}
